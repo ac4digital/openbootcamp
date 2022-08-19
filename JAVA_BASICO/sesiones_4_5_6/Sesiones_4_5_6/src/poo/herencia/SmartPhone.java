d33722608d48b2dd76bbc2d5c90f81aa527d2f93
Andres Cataño <acatano@ac4digital.com>
2022-08-18 23:06:42 -0500

    Entrega ejercicios sesion 4

--- JAVA_BASICO/sesiones_4_5_6/Sesiones_4_5_6/src/poo/herencia/SmartPhone.java
+++ JAVA_BASICO/sesiones_4_5_6/Sesiones_4_5_6/src/poo/herencia/SmartPhone.java
@@ -6,6 +6,10 @@ public class SmartPhone extends SmartDevice {
 
     boolean teclado;
 
+    public SmartPhone() {
+        
+    }
+
     public SmartPhone(double peso, double ancho, double alto, String marca, String modelo, String procesador, double precio, boolean teclado) {
         super(peso, ancho, alto, marca, modelo, procesador, precio);
         this.teclado = teclado;
--- JAVA_BASICO/sesiones_4_5_6/Sesiones_4_5_6/src/poo/herencia/SmartWatch.java
+++ JAVA_BASICO/sesiones_4_5_6/Sesiones_4_5_6/src/poo/herencia/SmartWatch.java
@@ -6,6 +6,10 @@ public class SmartWatch extends SmartDevice {
 
     String correa;
 
+    public SmartWatch() {
+
+    }
+
     public SmartWatch(double peso, double ancho, double alto, String marca, String modelo, String procesador, double precio, String correa) {
         super(peso, ancho, alto, marca, modelo, procesador, precio);
         this.correa = correa;
